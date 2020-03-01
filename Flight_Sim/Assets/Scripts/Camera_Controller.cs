f0c5d56007885163884ad8a423b4536993543aa2
MockingMonkey <mockingmonkeystudio@gmail.com>
2020-03-01 21:03:29 +0530

    Fix camera movement
    Fixed camera rotation and maintaing the position along the horizon line.

--- Flight_Sim/Assets/Scripts/Camera_Controller.cs
+++ Flight_Sim/Assets/Scripts/Camera_Controller.cs
@@ -9,22 +9,23 @@ public class Camera_Controller : MonoBehaviour
     #region Variables
     [Space]
     [Header("Object Refference :-")]
-    [SerializeField] private Transform _aircraft;
-    [SerializeField] private Camera _playerCam;
-    [SerializeField] private Transform _boreAim;
-    [SerializeField] private Transform _camAim;
+    [SerializeField] private Transform _aircraft = null;
+    [SerializeField] private Transform _mainCamera = null;
+    [SerializeField] private Camera _playerCam = null;
+    [SerializeField] private Transform _boreAim = null;
+    [SerializeField] private Transform _camAim = null;
     [Space]
     [Header("Text Spectre :-")]
-    [SerializeField] private Text _initialAcceleration;
-    [SerializeField] private Text _currentAcceleration;
-    [SerializeField] private Text _currentRotation;
+    [SerializeField] private Text _initialAcceleration = null;
+    [SerializeField] private Text _currentAcceleration = null;
+    [SerializeField] private Text _currentRotation = null;
     [Space]
     [Header("Accelerometer Tweaks :-")]
     [SerializeField] [Range(0, 1f)] private float _smooth = 0.4f;
     [SerializeField] [Range(0, 100f)] private float _sensitivity = 6f;
     [Space]
     [Header("Game HUD :-")]
-    [SerializeField] private RectTransform _boresight;
+    [SerializeField] private RectTransform _boresight = null;
     [Space]
     [Header("Camera Tweaks :-")]
     [SerializeField] [Range(0, 1f)] private float _rotationSlerp = 0.5f;
@@ -34,40 +35,24 @@ public class Camera_Controller : MonoBehaviour
     #endregion
 
     #region BuildIn Methods
-    void Start()
+    private void Start()
     {
         _initialAccln = Input.acceleration;
         _currentAccln = Vector3.zero;
 
-        _initialAcceleration.text = "Initial Acceleraion :-\n" 
-            + _initialAccln.x.ToString("#.##") + "\n" 
-            + _initialAccln.y.ToString("#.##") + "\n" 
-            + _initialAccln.z.ToString("#.##");
+        initialAcclnDisplay();
     }
 
-    void Update()
+    private void Update()
     {
         camFollow();
-
-
-        _currentAccln = Vector3.Lerp(_currentAccln, Input.acceleration - _initialAccln, Time.deltaTime / _smooth);
-
-        _currentAcceleration.text = "Current Acceleraion :-\n" 
-            + _currentAccln.x.ToString("#.##") + "\n" 
-            + _currentAccln.y.ToString("#.##") + "\n" 
-            + _currentAccln.z.ToString("#.##");
-
-        _newRotation.x = Mathf.Clamp(_currentAccln.x * _sensitivity, -1f, 1f);
-        _newRotation.z = Mathf.Clamp(_currentAccln.z * _sensitivity, -1f, 1f);
-
-        transform.Rotate(-_newRotation.z, _newRotation.x, 0f, Space.Self);
-
-        _currentRotation.text = "Current Rotation :-\n" 
-            + transform.localEulerAngles.x.ToString("#.##") 
-            + "\n" + transform.localEulerAngles.y.ToString("#.##") 
-            + "\n" + transform.localEulerAngles.z.ToString("#.##");
-
+        onGameGUI();
+        cameraRotation();
         updateGraphic();
+    }
+
+    private void FixedUpdate()
+    {
         aircraftRotation();
     }
     #endregion
@@ -78,10 +63,7 @@ public class Camera_Controller : MonoBehaviour
         _initialAccln = Input.acceleration;
         _currentAccln = Vector3.zero;
 
-        _initialAcceleration.text = "Initial Acceleraion :-\n" 
-            + _initialAccln.x.ToString("#.##") + "\n" 
-            + _initialAccln.y.ToString("#.##") + "\n" 
-            + _initialAccln.z.ToString("#.##");
+        initialAcclnDisplay();
     }
 
     private void camFollow()
@@ -105,9 +87,38 @@ public class Camera_Controller : MonoBehaviour
 
         Quaternion planeRotation = Quaternion.LookRotation(directionToFace);
         _aircraft.rotation = Quaternion.Slerp(_aircraft.rotation, planeRotation, Time.deltaTime * _rotationSlerp);
+    }
 
-        //_aircraft.rotation = Quaternion.LookRotation(directionToFace);
-        //_aircraft.rotation = Quaternion.Slerp(_aircraft.rotation, planeRotation, Time.deltaTime * 0.5f);
+    private void onGameGUI()
+    {
+        _currentAcceleration.text = "Current Acceleraion :-\n"
+            + _currentAccln.x.ToString("#.##") + "\n"
+            + _currentAccln.y.ToString("#.##") + "\n"
+            + _currentAccln.z.ToString("#.##");
+
+        _currentRotation.text = "Current Rotation :-\n"
+            + transform.localEulerAngles.x.ToString("#.##")
+            + "\n" + transform.localEulerAngles.y.ToString("#.##")
+            + "\n" + transform.localEulerAngles.z.ToString("#.##");
+    }
+
+    private void cameraRotation()
+    {
+        _currentAccln = Vector3.Lerp(_currentAccln, Input.acceleration - _initialAccln, Time.deltaTime / _smooth);
+
+        _newRotation.x = Mathf.Clamp(_currentAccln.x * _sensitivity, -1f, 1f);
+        _newRotation.z = Mathf.Clamp(_currentAccln.z * _sensitivity, -1f, 1f);
+
+        _mainCamera.Rotate(-_newRotation.z, 0f, 0f, Space.Self);
+        transform.Rotate(0f, _newRotation.x, 0f, Space.Self);
+    }
+
+    private void initialAcclnDisplay()
+    {
+        _initialAcceleration.text = "Initial Acceleraion :-\n"
+            + _initialAccln.x.ToString("#.##") + "\n"
+            + _initialAccln.y.ToString("#.##") + "\n"
+            + _initialAccln.z.ToString("#.##");
     }
     #endregion
 }
---
+++ Flight_Sim/Assets/Scripts/Flight_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flight_Controller : MonoBehaviour
+{
+    #region Variables
+    [Space]
+    [Header("Object Refference :-")]
+    [SerializeField] private Transform _originalAircraft = null;
+    [Space]
+    [Header("Flight Controls :-")]
+    [SerializeField] private AnimationCurve _aircraftBanking = AnimationCurve.Linear(0f, 0f, 1f, 30f);
+    [SerializeField] private float _offAngle = 30f;
+    #endregion
+
+    #region BuildIn Methods
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        realisticBanking();
+    }
+    #endregion
+
+    #region Custom Methods
+    private void realisticBanking()
+    {
+        float rotationAngle = _offAngle * -Input.acceleration.x;
+        float bodyRotation = _originalAircraft.rotation.z + rotationAngle;
+        Quaternion newRotation = Quaternion.Euler(0f, 0f, bodyRotation);
+        _originalAircraft.rotation = Quaternion.Slerp(_originalAircraft.rotation, transform.rotation * newRotation, Time.deltaTime * 10f);
+    }
+    #endregion
+}
